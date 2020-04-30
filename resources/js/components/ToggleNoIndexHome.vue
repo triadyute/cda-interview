629264f68b27cec4c4b18df6b451f6b00b651382
github_username <fabianc_henry@yahoo.com>
2020-04-30 08:16:12 -0500

    added toggle button to homepage form

--- resources/js/components/ToggleNoIndexHome.vue
+++ resources/js/components/ToggleNoIndexHome.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
-        <p>button</p>
+        <toggle-button
+        @change="onChangeEventHandler()">
+        </toggle-button>
     </div>
 </template>
 
 <script>
+    import { ToggleButton } from 'vue-js-toggle-button';
+    Vue.use(ToggleButton);
     export default {
+        components: {
+            ToggleButton
+        },
+        methods: {
+            onChangeEventHandler(){
+                console.log('toggled');
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
